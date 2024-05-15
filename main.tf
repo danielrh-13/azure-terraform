643b235257bad355e722a460bac657f58d7bfa0e
danielrh-13 <165545729+danielrh-13@users.noreply.github.com>
2024-05-15 08:15:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   features {}  
   skip_provider_registration = true
-  client_id       = var.client_id
+/*  client_id       = var.client_id
   client_secret   = var.client_secret
   subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  tenant_id       = var.tenant_id */
 }
 
 module "networking" {
