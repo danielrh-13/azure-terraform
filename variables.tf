7dc2a2d80b7d2ad673237870f03053991814d99c
danielrh-13 <165545729+danielrh-13@users.noreply.github.com>
2024-05-15 07:27:56 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,19 @@ variable "locations" {
   type = string
   default = "East US"
 }
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
